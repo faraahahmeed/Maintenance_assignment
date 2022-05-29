7b6ec3f8c926b411c7ff6eb1f756b1eae8e0c6a5
EsraaAshrafFayez <93885354+EsraaAshrafFayez@users.noreply.github.com>
2022-05-29 21:58:47 +0200

    fixed duplicated code

--- Notifications/src/consumer/TA.java
+++ Notifications/src/consumer/TA.java
@@ -22,34 +22,18 @@ public class TA {
 	}
 
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
 
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
 
 	public String getDepartment() {
 		return department;
-	}
-
-	public void setDepartment(String department) {
-		this.department = department;
 	}
 	
 	public void notifyTA(String message) {
